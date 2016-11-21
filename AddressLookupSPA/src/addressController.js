b4fcd056dd9afaa381c6e9c1b241e2be7b36d13d
Ad van Bergen <ad.van.bergen@gmail.com>
2016-11-21 20:47:36 +0100

    repopulate solution

---
+++ AddressLookupSPA/src/addressController.js
@@ -0,0 +1,13 @@
+﻿// controller for address lookup
+addressController.$inject = ['addressFactory'];
+
+function addressController(addressFactory) {
+  var vm = this;
+  addressFactory.getAddress()
+  .success(function (address) {
+    vm.address = address;
+  })
+  .error(function (err) {
+    alert('Something went wrong in the address lookup!')
+  });
+}
---
+++ AddressLookupSPA/src/addressFactory.js
@@ -0,0 +1,34 @@
+﻿//http://api.postcodedata.nl/v1/postcode/?postcode=4851AH&streetnumber=16&ref=domeinnaam.nl&type=json
+ 
+//expected response:
+//  {
+//    "status": "ok",
+//    "details": [{
+//      "street": "Rooiakker",
+//      "city": "Ulvenhout",
+//      "municipality": "Breda",
+//      "province": "Noord-Brabant",
+//      "postcode": "4851 AH",
+//      "pnum": "4851",
+//      "pchar": "AH",
+//      "rd_x": "114263.25740000000000000000",
+//      "rd_y": "395974.31535000000000000000",
+//      "lat": "51.5512518032450",
+//      "lon": "4.7998622348226"
+//    }]
+//  }
+ 
+
+addressFactory.$inject = ['$http', 'GLOBALS'];
+
+function addressFactory($http, GLOBALS) {
+  var factory = {};
+  factory.getAddress = function () {
+    return
+    return $http({
+      method: 'GET',
+      url: GLOBALS.addressUrl
+    });
+  };
+  return factory;
+}
