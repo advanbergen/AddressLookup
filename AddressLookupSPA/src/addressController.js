8ea1e35ac725244aa296d66a5fea86ca5ef8f20b
Ad van Bergen <ad.van.bergen@gmail.com>
2016-11-27 17:18:10 +0100

    added google map functionality, not yet coupled to the lookup result coordinates

--- AddressLookupSPA/src/addressController.js
+++ AddressLookupSPA/src/addressController.js
@@ -1,16 +1,19 @@
 ﻿// controller for address lookup
-addressController.$inject = ['addressFactory'];
 
-function addressController(addressFactory) {
-  var vm = this;
-  vm.getAddress = function () {
-    addressFactory.getAddress(vm.postcode, vm.streetnumber)
-    .success(function (address) {
-      vm.address = address;
-    })
-    .error(function (err) {
-      alert('Something went wrong in the address lookup!');
-    });
-  }
-}
+addressController.$inject = ['addressFactory']; //, 'googleMap'
+//(function () {
 
+    function addressController(addressFactory) {
+        var vm = this;
+        vm.getAddress = function () {
+            addressFactory.getAddress(vm.postcode, vm.streetnumber)
+            .success(function (address) {
+                vm.address = address;
+            })
+            .error(function (err) {
+                alert('Something went wrong in the address lookup!');
+            });
+        }
+    }
+
+//}());
--- AddressLookupSPA/src/addressFactory.js
+++ AddressLookupSPA/src/addressFactory.js
@@ -1,34 +1,27 @@
-﻿//http://api.postcodedata.nl/v1/postcode/?postcode=4851AH&streetnumber=16&ref=domeinnaam.nl&type=json
- 
-//expected response:
+﻿//(function () {
+    addressFactory.$inject = ['$http', 'GLOBALS'];
+    function addressFactory($http, GLOBALS) { 
+        var factory = {};
+        factory.getAddress = function (postcode, streetnumber) {
+            var combinedUrl = GLOBALS.addressUrl+'?postcode=' + postcode + '&streetnumber=' + streetnumber + '&ref=domeinnaam.nl&type=json';
+            return $http({
+                method: 'GET',
+                url: combinedUrl
+            });
+        };
+        return factory;
+    }
+//})();
+
+
+//Example url for address lookup:
+// http://api.postcodedata.nl/v1/postcode/?postcode=4851AH&streetnumber=16&ref=domeinnaam.nl&type=json
+
+//Expected response (json):
 //  {
 //    "status": "ok",
 //    "details": [{
-//      "street": "Rooiakker",
-//      "city": "Ulvenhout",
-//      "municipality": "Breda",
-//      "province": "Noord-Brabant",
-//      "postcode": "4851 AH",
-//      "pnum": "4851",
-//      "pchar": "AH",
-//      "rd_x": "114263.25740000000000000000",
-//      "rd_y": "395974.31535000000000000000",
-//      "lat": "51.5512518032450",
-//      "lon": "4.7998622348226"
-//    }]
+//      "street": "Rooiakker","city": "Ulvenhout","municipality": "Breda","province": "Noord-Brabant",
+//      "postcode": "4851 AH","pnum": "4851","pchar": "AH",
+//      "rd_x": "114263.25740000000000000000","rd_y": "395974.31535000000000000000","lat": "51.5512518032450","lon": "4.7998622348226"}]
 //  }
- 
-
-addressFactory.$inject = ['$http', 'GLOBALS'];//, postcode, streetnumber];
-
-function addressFactory($http, GLOBALS) { //, postcode, streetnumber) {
-  var factory = {};
-  factory.getAddress = function (postcode, streetnumber) {
-    var combinedUrl = GLOBALS.addressUrl+'?postcode=' + postcode + '&streetnumber=' + streetnumber + '&ref=domeinnaam.nl&type=json';
-    return $http({
-      method: 'GET',
-      url: combinedUrl
-    });
-  };
-  return factory;
-}
