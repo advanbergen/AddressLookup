cd57d4a7e6d6fecb4e04376e5a336ed5e8427367
Ad van Bergen <ad.van.bergen@gmail.com>
2017-04-30 12:01:34 +0200

    log

--- src/ngApp.js
+++ src/ngApp.js
@@ -9,7 +9,7 @@
 
 function myClock() {
   var directive = {
-    template: '<div>{{ vm.time | date: "HH:mm:ss" }}</div>',
+    template: '<h2>{{ vm.time | date: "HH:mm:ss" }}</h2>',
     controller: clockController,
     controllerAs: 'vm'
   }
@@ -35,6 +35,7 @@ function myGoogleMap() {
 // adding <div id="map"></div> to the index.html shows the map with the specified coordinates
 var map;
 function initMap() {
+  console.log('initMap is called', map);
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 51.09, lng: 5.11 }, // centraal station utrecht
         //center: { lat: 	51.5512518032450, lng: 4.7998622348226 }, // rooiakker 16 ulvenhout
