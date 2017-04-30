475ceb80f27d25f4792425f473b7a1006c5fb733
Ad van Bergen <ad.van.bergen@gmail.com>
2017-04-30 16:17:25 +0200

    prepared for showing google map with new coordinates from API call

--- src/ngApp.js
+++ src/ngApp.js
@@ -26,7 +26,7 @@ function clockController($interval){
 
 function myGoogleMap() {
   var directive = {
-    template: '<div id="map">Showing on Google Map, latitude:{{vm.latitude}}, longitude:{{vm.longitude}}</div>',
+    template: '<div>Showing on Google Map, latitude:{{vm.latitude}}, longitude:{{vm.longitude}}, zoom:{{vm.zoom}}</div>',
     controller: googleMapController,
     controllerAs: 'vm',
     bindToController: true,
@@ -44,15 +44,27 @@ function googleMapController() {
   console.log('latitude:',vm.latitude);
   console.log('longitude:',vm.longitude);
   console.log('zoom:',vm.zoom);
+  if (!!vm.latitude)
+    initMap2(vm.latitude, vm.longitude, vm.zoom);
+}
+
+function initMap() {
+  console.log('initMap called');
+  initMap2(51.09, 5.11, 7);// centraal station utrecht
 }
 
 // adding <div id="map"></div> to the index.html shows the map with the specified coordinates
 var map;
-function initMap() {
-  console.log('initMap is called', map);
+function initMap2(latitude, longitude, zoom) {
+    console.log('initMap2 called');
+    console.log('initMap2 latitude:',latitude);
+    console.log('initMap2 longitude:',longitude);
+    console.log('initMap2 zoom:',zoom);
+
     map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 51.09, lng: 5.11 }, // centraal station utrecht
-        //center: { lat: 	51.5512518032450, lng: 4.7998622348226 }, // Rooiakker 16, Ulvenhout
-        zoom: 7 //17
+        // center: { lat: 	51.5512518032450, lng: 4.7998622348226 }, // Rooiakker 16, Ulvenhout 
+        center: { lat: latitude, lng: longitude}, 
+        zoom: zoom //17
     });
+    console.log('map is defined:', map);
 }
